fa10c2a18dc743f540fed1b2178c9c0d08d52a75
Mohammad Arif <arif_iq@yahoo.co.in>
2022-05-18 12:29:46 +0530

    GitHub users fetched and painted on UserResults page after setting up the GitHub Token to increase the rate limit.

---
+++ src/components/users/UserItem.tsx
@@ -0,0 +1,28 @@
+type UserItemProps = {
+  user: {
+    login: string;
+    avatar_url: string;
+  };
+  key: number;
+};
+
+const UserItem = ({ user: { login, avatar_url } }: UserItemProps) => {
+  return (
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div>
+          <div className='avatar'>
+            <div className='rounded-full shadow w-14 h-14'>
+              <img src={avatar_url} alt='Profile' />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className='card-title'>{login}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
